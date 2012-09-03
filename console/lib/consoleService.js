913dc815b7b32c28f45d948d3ea8b87ad52a1813
chang <changchang005@gmail.com>
2012-09-03 11:12:04 +0800

    fix bugs

--- console/lib/consoleService.js
+++ console/lib/consoleService.js
@@ -29,6 +29,7 @@ var Service = function(opts) {
 			type: this.type
 		});
 	}
+	this.queue = [];
 };
 
 module.exports = Service;
@@ -51,7 +52,11 @@ pro.stop = function() {
 };
 
 pro.register = function(moduleId, module) {
-	this.modules[moduleId] = registerRecord(module);
+	var record = registerRecord(module);
+	this.modules[moduleId] = record;
+	if(module.interval) {
+		doIntervalRecord(service, record);
+	}
 };
 
 pro.enable = function(moduleId) {
@@ -105,4 +110,17 @@ var registerRecord = function(module) {
 		module: module, 
 		enable: true
 	};
+};
+
+var doIntervalRecord = function(service, record) {
+	var queue = service.queue;
+	if(service.type !== 'master' && record.module.type === 'push') {
+		// push mode for monitor
+		queue.push(record);
+	} else if(service.type === 'master' && record.module.type !== 'pull') {
+		// pull mode for master by default
+		queue.push(record);
+	} else {
+		console.warn('ignore invalid interval record for %j, %j', service.type, record.module.type);
+	}
 };
