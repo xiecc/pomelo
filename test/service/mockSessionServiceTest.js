b0d0802f3f907462bc333d808148abf5f8d4378a
chang <changchang005@gmail.com>
2012-11-12 14:58:28 +0800

    clean codes

--- test/service/mockSessionServiceTest.js
+++ test/service/mockSessionServiceTest.js
@@ -4,12 +4,12 @@ var should = require('should');
 describe('mockSessionServiceTest', function(){
 	it('session create should be ok!', function(done){
 		var session = mockSessionService.createSession({uid:1001, key: 'xcc123go'});
-        session.userLogined(1001).should.not.be.ok;
-        
-        var expSession = session.exportSession();
-        
-        should.equal(expSession.uid, session.uid);
-        done();
+		session.bind(1001).should.not.be.ok;
+
+		var expSession = session.exportSession();
+
+		should.equal(expSession.uid, session.uid);
+		done();
 	});
 });
 
